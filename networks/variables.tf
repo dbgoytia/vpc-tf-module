3766bb1fd1350eace97b560f997d1d3190e8357c
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-19 19:59:20 -0600

    deleted old files

--- networks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr_block" {
-  description = "CIDR block for our VPC"
-  type        = string
-}
-
-variable "cidr_block_subnet" {
-  description = "Subnet 1 CIDR block"
-  type        = string
-}
