8f1b406b19ce5a0f4a71b3387c908fb818c1252d
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-27 15:35:08 -0600

    added output for SUBNET CIDRS

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
-output "VPC_ID" {
+output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
+output "public_subnets_cidrs" {
+  description = "List with the Public Subnets cidrs"
+  value       = aws_subnet.public_subnets.*
+}
