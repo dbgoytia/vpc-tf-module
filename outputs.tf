3963e36130c86272e408f56af9ca9af163d70e2f
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-27 15:50:48 -0600

    Changed the output form CIDR blocks to ID's of the subnet.

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "public_subnets_cidrs" {
-  description = "List with the Public Subnets cidrs"
-  value       = aws_subnet.public_subnets.*.cidr_block
+output "public_subnets_ids" {
+  description = "List with the Public Subnets ids"
+  value       = aws_subnet.public_subnets.*.id
 }
