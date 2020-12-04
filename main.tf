0b59d5d9e8655461234cbece01f220fc9aebab18
Diego Goytia <diego.canizales1995@gmail.com>
2020-12-04 13:15:26 -0600

    Added the functionality to turn/off single-nat gateways

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+# --------------------------------------
+# AWS Backend configuration
+# --------------------------------------
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      region = "us-east-1"
+    }
+  }
+  backend "s3" {
+    bucket = "691e4876-f921-0542-c9c7-0989c184fe8c-backend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# --------------------------------------
+# AWS Provider
+# --------------------------------------
+provider "aws" {
+  region = var.region
+}
+
 #------------------------------------------------------------------------------
 # AWS Virtual Private Network
 #------------------------------------------------------------------------------
@@ -13,9 +37,67 @@ resource "aws_vpc" "vpc" {
 #------------------------------------------------------------------------------
 # AWS Internet Gateway
 #------------------------------------------------------------------------------
-resource "aws_internet_gateway" "ig" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
+
+#------------------------------------------------------------------------------
+# AWS Subnets - Elastic IP
+#------------------------------------------------------------------------------
+resource "aws_eip" "nat_gw_eip" {
+  count = var.create_natted_subnet == true ? 1 : 0
+  vpc   = true
+}
+#------------------------------------------------------------------------------
+# AWS Subnets - NAT gateway
+#------------------------------------------------------------------------------
+# For now, attach a single NAT Gateway to your first private subnet
+resource "aws_nat_gateway" "gw" {
+  count         = var.create_natted_subnet ? 1 : 0
+  allocation_id = aws_eip.nat_gw_eip[0].id
+  subnet_id     = aws_subnet.public_subnets[0].id
+  depends_on    = [aws_subnet.public_subnets]
+}
+
+
+#------------------------------------------------------------------------------
+# AWS Route Table (Private Routes) - As many as the number of NAT Gateways
+#------------------------------------------------------------------------------
+resource "aws_route_table" "private" {
+  count  = var.create_natted_subnet ? 1 : 0
+  vpc_id = aws_vpc.vpc.id
+}
+
+#------------------------------------------------------------------------------
+# AWS Subnets - Private
+#------------------------------------------------------------------------------
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnets[count.index]
+}
+
+# AWS Route
+resource "aws_route" "private_nat_gateway" {
+  count  = var.create_natted_subnet ? 1 : 0
+
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.gw.*.id, count.index)
+  
+}
+
+#------------------------------------------------------------------------------
+# Route Table association (Private)
+#------------------------------------------------------------------------------
+resource "aws_route_table_association" "private" {
+  count     = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+  subnet_id = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(
+    aws_route_table.private.*.id, 0
+  )
+}
+
 
 #------------------------------------------------------------------------------
 # AWS Subnets - Public
@@ -36,8 +118,8 @@ resource "aws_route_table" "internet_route" {
 
   route {
     # Allow traffic from our internet gateway
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ig.id
+    cidr_block     = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -5,5 +5,20 @@ variable "vpc_cidr_block" {
 
 variable "public_subnets_cidrs" {
   type        = list
-  description = "List of CIDRs to use"
+  description = "List of private subnets to create in CIDR notation."
+}
+
+variable "create_natted_subnet" {
+  description = "If you set this variable to true, the module will deploy a VPC NAT Gateway in front of the public subnet (Will assign an Elastic IP)"
+  type        = bool
+}
+
+variable "private_subnets" {
+  description = "Create a variable number of private subnets"
+  type        = list
+}
+
+variable "region" {
+  description = "Region to be used for deployment"
+  type        = string
 }
