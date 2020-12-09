76fe6baa858a8aadecc86937bee45b7601436fba
Diego Goytia <diego.canizales1995@gmail.com>
2020-12-09 14:48:58 -0600

    Added output for default security group.

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,8 @@ output "private_subnets_cidrs" {
   description = "List with the Public Subnets ids"
   value       = aws_subnet.private_subnets.*.cidr_block
 }
+
+output "default_sg" {
+  description = "Output VPC default security group"
+  value       = aws_vpc.vpc.default_security_group_id
+}
